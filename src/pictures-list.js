d0fed3b2c59a85bb0afdf327587d55b92e85e7a8
Ddddddaasnnnniiiilll <117436063+Ddddddaasnnnniiiilll@users.noreply.github.com>
2022-12-30 19:22:06 +1000

    Update pictures-list.js

--- src/pictures-list.js
+++ src/pictures-list.js
@@ -1,11 +1,24 @@
 const picturesListElement = document.querySelector('.pictures');
 const previewModalElement = document.querySelector('.big-picture')
 const previewModalCloseButtonElement = document.querySelector('.big-picture__cancel')
+const commentsListElement = document.querySelector('.social__comments');
+
 const pictureTempLateElement = document.getElementById('picture')
     .content
     .querySelector('.picture');
 
+const commentTempLateElement = document.getElementById('comment')
+    .content
+    .querySelector('.social__comment');
 
+const renderCommentsList = (comments) => {
+    for (const comment of comments) {
+        const commentElement = commentTempLateElement.cloneNode(true);
+        commentElement.querySelector('.social__text').textContent = comment.message;
+
+        commentsListElement.append(commentElement);
+    }
+}
 
 const renderPicturesList = (pictures) => {
     for (const picture of pictures) {
@@ -16,24 +29,34 @@ const renderPicturesList = (pictures) => {
         pictureElement.querySelector('.picture__likes').textContent = picture.likes;
         pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
 
-        picturesListElement.append(pictureElement)
+        picturesListElement.append(pictureElement);
+    }
 
-        pictureElement.addEventListener('click', function () {
-            const id = pictureElement.dataset.id;
+    picturesListElement.addEventListener('click', function (evt) {
+        const picture = evt.target.closest(".picture");
 
+        if (picture) {
+            const id = picture.dataset.id;
             const pictureItem = pictures.find(function (picture) {
                 return picture.id === Number(id);
             });
 
             previewModalElement.querySelector('img').src = pictureItem.url;
+            renderCommentsList(pictureItem.comments);
             previewModalElement.classList.remove('hidden');
-        });
-    }
+        }
+    });
+
     previewModalCloseButtonElement.addEventListener('click', function () {
         previewModalElement.classList.add('hidden')
     });
 
-    
+    document.addEventListener('keydown', function (evt) {
+        if (evt.code === "Escape") {
+            console.log(evt);
+            previewModalElement.classList.add('hidden')
+        }
+    });
 };
 
 export {renderPicturesList};
